7bfb8199e99387168b1d56d3492982b95ad06ea1
popoolahussein <popoolakehindehussein@gmail.com>
2024-10-08 10:42:08 +0100

    feat: ensured the persistence of cart items in CartContext

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCart = localStorage.getItem('cartItems');
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <CartContext.Provider value={{ cartItems, setCartItems }}>

a2a4ff584ad0e694cdb63c29264189d59b5cfdfe
popoolahussein <popoolakehindehussein@gmail.com>
2024-10-13 18:00:18 +0100

    refactor: transmitted from React Context to Redux for global state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useContext } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import Shop from './Shop.jsx';
 import Cart from './Cart.jsx';
 import Navbar from './Navbar.jsx';
-import { CartContext } from './CartContext.jsx';
+import store from './store';
 
 const App = () => {
-  const { cartItems, setCartItems } = useContext(CartContext);
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar cartItems={cartItems} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop cartItems={cartItems} setCartItems={setCartItems} />} />
-          <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 };
 

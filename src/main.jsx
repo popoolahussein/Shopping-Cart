afc488c2204d1abd7ab666345e46261f9864ba37
popoolahussein <popoolakehindehussein@gmail.com>
2024-10-13 18:08:49 +0100

    refactor: integrated Redux store and Provider into app entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import the Provider
+import store from './store'; // Import your Redux store
 import App from './App.jsx';
-import { CartProvider } from './CartContext.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CartProvider>
+    <Provider store={store}> {/* Wrap your App with Provider */}
       <App />
-    </CartProvider>
+    </Provider>
   </StrictMode>,
 );
